ccf773865fa72e467357bb0e0a091291d6890054
DvlPrSujalPatel <sujal7455@gmail.com>
2024-10-16 21:05:15 +0530

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <h1>Heloo</h1>
-    </>
-  )
+  return <div>
+    
+  </div>
 }
 
 export default App
